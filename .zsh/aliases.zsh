c171631d2df8d7d59d2bd4ebfaea44045ff7cd8b
Mario Da Silva <mariod2@discovery.co.za>
2024-06-17 11:35:53 +0200

    Add all my dotfiles from my mac after set up so I can pull them to what ever server I want

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,17 @@
+# Alias
+# ---
+#
+alias lb="liquibase"
+
+# mac OS shortcuts
+alias code="open -a 'Visual Studio Code'"
+
+# ALIAS COMMANDS
+alias ls="eza --icons --group-directories-first"
+alias ll="eza --icons --group-directories-first -l"
+alias tree="eza --tree"
+alias grep='grep --color'
+alias python='/usr/local/bin/python3'
+alias taw='cd /Users/mario15/do/workspaces/Taw'
+alias vdi='cd /Users/mario15/do/workspaces/VDI'
+alias avis='cd /Users/mario15/do/workspaces/Avis'
